bf17f5e80872ba84e9af422cef9bfc289acf10e6
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-09-28 02:28:44 -0400

    fixed additional bug with nav-bar

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ import "./styles/style.scss";
 const toggle = document.querySelector("#toggle");
 const menu = document.querySelector(".menu-links");
 const icon = document.querySelector(".hamburger-icon");
+let anchors = document.getElementsByTagName("a");
 
 function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
 
+for (var i = 0; i < anchors.length; i++) {
+  anchors[i].addEventListener("click", toggleMenu);
+}
 toggle.addEventListener("click", toggleMenu);
 
 document.onclick = function (e) {
