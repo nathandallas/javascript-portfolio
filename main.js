e94c848bf556ea24f19c445124fb50dcbd50e2f7
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-09-28 02:05:18 -0400

    styled about, fixed bug in mobile nav

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import "./styles/style.scss";
 
+// MOBILE NAV
+
 const toggle = document.querySelector("#toggle");
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
 
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-  
 }
 
 toggle.addEventListener("click", toggleMenu);
+
+document.onclick = function (e) {
+  if (!menu.contains(e.target) && !icon.contains(e.target)) {
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  }
+};
