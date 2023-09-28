541478f4fcaf4240a6eca0c15f27cefdc3540f19
Nathan <87546582+nathandallas@users.noreply.github.com>
2023-09-28 00:04:08 -0400

    section structure and projects

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import "./style.scss";
+import "./styles/style.scss";
 
-const toggle = document.querySelector("#toggle")
-
+const toggle = document.querySelector("#toggle");
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  
 }
 
 toggle.addEventListener("click", toggleMenu);
